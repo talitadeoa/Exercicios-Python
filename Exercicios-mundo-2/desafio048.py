497f6a74c2f8bbf92a4ff980697beb7ee8c3b687
Talita Araujo <edtaraujo@gmail.com>
2020-12-21 01:03:48 -0300

    .

--- Exercicios-mundo-2/desafio048.py
+++ Exercicios-mundo-2/desafio048.py
@@ -1,8 +1,10 @@
 #Um programa que calcula a soma de todos os números ímpares entre 1 e 500
 
-s = 0
-for i in range (1, 501):
-    if i%2 != 0:
-        s = i+i
+soma = 0
+count = 0
+for i in range (1, 501, 2):
+    if i % 3 == 0:
+        soma+=i
+        count+=1
 
-print('A soma de todos os números ímpares entre 1 e 500 é {}'.format(s))
+print('A soma dos {} valores ímpares múltiplos de 3 entre 1 e 500 é {}'.format(count,soma))
---
+++ Exercicios-mundo-2/desafio049.py
@@ -0,0 +1,3 @@
+#Refaça a tabuada do desafio 009 utilizando o laço for
+
+#kkk fiz o desafio 009 já utilizando o laço for
