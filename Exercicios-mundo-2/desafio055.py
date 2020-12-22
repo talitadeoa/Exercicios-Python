830155040d03dd85a5fe08babf66332a3430a747
Talita Araujo <edtaraujo@gmail.com>
2020-12-21 23:28:46 -0300

    .

--- Exercicios-mundo-2/desafio055.py
+++ Exercicios-mundo-2/desafio055.py
@@ -9,8 +9,18 @@ menor = 0
 for i in range(0,5):
     peso = float(input('Qual o peso da {}ª pessoa? '.format(pessoa)))
     pessoa += 1
-    
-if
+    pesos.append(peso)
+
+if pesos[0] > pesos[1] and pesos[0] > pesos[2] and pesos[0] > pesos[3] and pesos[0] > pesos[4]:
+    print('O peso {} é o maior peso da lista'.format(pesos[1]))
+if pesos[1] > pesos[0] and pesos[1] > pesos[2] and pesos[1] > pesos[3] and pesos[1] > pesos[4]:
+    print('O peso {} é o maior peso da lista'.format(pesos[2]))
+if pesos[2] > pesos[1] and pesos[2] > pesos[3] and pesos[2] > pesos[4] and pesos[3] > pesos[5]:
+    print('O peso {} é o maior peso da lista'.format(pesos[3]))
+if pesos[4] > pesos[1] and pesos[4] > pesos[2] and pesos[4] > pesos[3] and pesos[4] > pesos[5]:
+    print('O peso {} é o maior peso da lista'.format(pesos[4]))
+if pesos[5] > pesos[1] and pesos[5] > pesos[2] and pesos[5] > pesos[3] and pesos[5] > pesos[4]:
+    print('O peso {} é o maior peso da lista'.format(pesos[5])) 
 
 print(peso)
-
+print(pesos)
---
+++ Exercicios-mundo-2/fdfkjg.py
@@ -0,0 +1,10 @@
+if pesos[1] > pesos[2] and pesos[1] > pesos[3] and pesos[1] > pesos[4] and pesos[1] > pesos[5]:
+    print('O peso {} é o maior peso da lista'.format(pesos[1]))
+if pesos[2] > pesos[1] and pesos[2] > pesos[3] and pesos[2] > pesos[4] and pesos[2] > pesos[5]:
+    print('O peso {} é o maior peso da lista'.format(pesos[2]))
+if pesos[3] > pesos[1] and pesos[3] > pesos[2] and pesos[3] > pesos[4] and pesos[3] > pesos[5]:
+    print('O peso {} é o maior peso da lista'.format(pesos[3]))
+if pesos[4] > pesos[1] and pesos[4] > pesos[2] and pesos[4] > pesos[3] and pesos[4] > pesos[5]:
+    print('O peso {} é o maior peso da lista'.format(pesos[4]))
+if pesos[5] > pesos[1] and pesos[5] > pesos[2] and pesos[5] > pesos[3] and pesos[5] > pesos[4]:
+    print('O peso {} é o maior peso da lista'.format(pesos[5]))
