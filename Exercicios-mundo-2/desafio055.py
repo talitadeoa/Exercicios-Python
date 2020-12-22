e2a4e9bf4a6136a3599203a69cd39110072a791b
Talita Araujo <edtaraujo@gmail.com>
2020-12-21 21:23:06 -0300

    .

--- Exercicios-mundo-2/desafio055.py
+++ Exercicios-mundo-2/desafio055.py
@@ -1,5 +1,6 @@
 #Um programa que lê o peso de 5 pessoas e informa qual o maior e menor peso
 
+pesos = []
 peso = 0
 pessoa = 1
 maior = 0
@@ -8,4 +9,8 @@ menor = 0
 for i in range(0,5):
     peso = float(input('Qual o peso da {}ª pessoa? '.format(pessoa)))
     pessoa += 1
+    
+if
+
+print(peso)
 
