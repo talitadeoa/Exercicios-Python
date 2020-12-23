ebe65a63ab57eea7b961a9e6066f0d2f0c2e99e4
Talita Araujo <edtaraujo@gmail.com>
2020-12-23 13:53:44 -0300

    .

--- Exercicios-mundo-2/desafio063.py
+++ Exercicios-mundo-2/desafio063.py
@@ -1,4 +1,17 @@
 #Programa que lê um número inteiro qualquer e mostra uma sequência fibonacci
 
-num =
-qt =
+num = int(input('Digite um número inteiro qualquer_'))
+qt = int(input('Quantos elementos você deseja ver?_'))
+pro = 0
+a = num - 1
+aa = num - 2
+
+while num < 1 or a < 1 or aa < 1:
+    num += 1
+
+while qt > 0:
+    print(f'{num}', end=' ')
+    num += a + aa
+    qt -= 1
+
+
