30f097bebfe838c749fe8840483568c272647fdb
Talita Araujo <edtaraujo@gmail.com>
2020-12-23 18:06:32 -0300

    .

---
+++ Exercicios-mundo-2/desafio064b.py
@@ -0,0 +1,21 @@
+#Um programa que lê vários números até receber 999(condição de parada) Mostra quantos valores foram inseridos e a soma deles
+
+num = 0
+count= 0
+all = []
+sum1 = 0
+sum2 = 0
+vav = 0 
+num = int(input('Digite um número inteiro qualquer (999 para parar) '))
+
+while num != 999:
+    count += 1
+    sum1 += num 
+    all.append(num)
+   
+    while vav < (len(all)):
+        sum2 += all[vav] 
+        vav += 1
+    num = int(input('Digite um número inteiro qualquer (999 para parar) '))
+
+print(f'Ok, você inseriu {count} valores, sendo eles {all} a soma total é {sum1}')
