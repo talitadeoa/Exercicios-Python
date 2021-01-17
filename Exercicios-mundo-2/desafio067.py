2c011d04091230f121e38792864d7cab18d6f52b
Talita Araújo <55101326+talitadeoa@users.noreply.github.com>
2021-01-17 15:58:04 -0300

    Update desafio067.py

--- Exercicios-mundo-2/desafio067.py
+++ Exercicios-mundo-2/desafio067.py
@@ -1,16 +1,17 @@
 #Um programa que mostra atabuada de um inteiro qualquer digitado pelo usuário, o programa é encerrado quando o valor inserido for negativo
 
-n = count = result = 0
+n = result = 0
+count = 0
 
 while True:
     n = int(input('Quer ver a tabuada de qual valor? '))
-    if n < 0:
+    if n <= 0:
         break
-    print(f'\n A tabuada de {n} é:')
-    for i in range (0,11):
+    print(f'\nA tabuada de {n} é:')
+    while count <= 10:
+        result = n*count
         print(f'{n} x {count} = {result}')
         count += 1
-        result = n*count
     print('-'*20)
     count = 0
 print('\nEncerrando calculadora')
