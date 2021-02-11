24444efcee074457bce8ce7ca7c902a6bd8cc349
Talita Araújo <55101326+talitadeoa@users.noreply.github.com>
2021-02-11 17:40:02 -0300

    Update desafio070.py

--- Exercicios-mundo-2/desafio070.py
+++ Exercicios-mundo-2/desafio070.py
@@ -1,2 +1,11 @@
 #Um programa que cria uma lista de compras com nome e preço e ao final informa o total gasto nas compras, quantos produtos custam mais de R$1000 e qual o produto mais barato
 
+shopping = []
+product = []
+price = []
+mt1000 = []
+cheapest = 0
+stop = False
+
+while stop is not False:
+    
