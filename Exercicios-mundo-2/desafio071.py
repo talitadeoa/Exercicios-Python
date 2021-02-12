92bcc4f04f179e640f3398622a8461bd6c51a2b5
Talita Araújo <55101326+talitadeoa@users.noreply.github.com>
2021-02-12 01:42:45 -0300

    Update desafio071.py

--- Exercicios-mundo-2/desafio071.py
+++ Exercicios-mundo-2/desafio071.py
@@ -5,7 +5,15 @@ value = bill50 = bill20 = bill10 = bill1 = 0
 
 while value == 0:
     value = int(input('Qual o valor que deseja sacar? R%'))
-    while value/50 >= 1:
+    while value/50 > 0:
         bill50 = value//50
         value = value - bill50*50
+    while value/20 > 0:
+        bill20 = value//20
+        value = value - bill20*20
+    while value/10 > 0:
+        bill10 = value//10
+        value = value - bill10*10
+    while value/1 > 0:
+
 
