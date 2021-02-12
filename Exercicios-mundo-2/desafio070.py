d9ed27f6be63f879535d857e2cf974e689c0b029
Talita Araújo <55101326+talitadeoa@users.noreply.github.com>
2021-02-12 16:50:27 -0300

    Update desafio070.py

--- Exercicios-mundo-2/desafio070.py
+++ Exercicios-mundo-2/desafio070.py
@@ -19,12 +19,12 @@ while True:
     else:
         if price < cheapest:
             cheapest = count
-    stop = input('Deseja cadastrar mais um produto? [S/N] ').upper()
+    stop = ''
     while stop not in ['S','N']:
         stop = input('Deseja cadastrar mais um produto? [S/N] ').upper()
-    if stop == 'S':
-        count+=1
     if stop == 'N':
         break
 
-print(f'O total das compras foi: R${sum(prices):.2f} \n{len(mt1000)} Produtos custaram mais de R$1000 \nO produto mais barato foi {shopping[cheapest]} custando {prices[count-1]}')        
+print(f'O total das compras foi: R${sum(prices):.2f} \n{len(mt1000)} Produtos custaram mais de R$1000 \nO produto mais barato foi {shopping[cheapest]} custando ')        
+
+#{prices[cheapest-1]}
