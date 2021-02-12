184a4bdc8a89589e05ceb4f3fab208e735c6042e
Talita Araújo <55101326+talitadeoa@users.noreply.github.com>
2021-02-12 16:27:33 -0300

    .

--- Exercicios-mundo-2/desafio069.py
+++ Exercicios-mundo-2/desafio069.py
@@ -1,23 +1,20 @@
 #Um programa que lê idade e sexo de várias pessoas no final informa quantos sao maiores de 18, quantos são homens e quantas mulheres tem menos de 20 anos
 
 p = 1
-#age = 0
 ages = []
-#gender = ''
 genders = []
 p18 = []
 men = []
 wlt20 = []
-stop = False
 
-while stop is not True:
+while True:
     age = int(input(f'Digite a idade da {p}ª pessoa: '))
     while age < 0:
         age = int(input(f'Digite a idade da {p}ª pessoa: '))        
     ages.append(age)
-    gender = (input(f'Digite o sexo da {p}ª pessoa: [M/F] ').upper())
+    gender = ''
     while gender not in ['M','F']:
-        gender = (input(f'Digitde o sexo da {p}ª pessoa: [M/F] ').upper())
+        gender = (input(f'Digitde o sexo da {p}ª pessoa: [M/F] ').upper().strip()[0])
     genders.append(gender)
 
     if gender == 'M':
@@ -26,14 +23,13 @@ while stop is not True:
         p18.append(p)
     if gender == 'F' and age < 20:
         wlt20.append(p)
-
+    stop = ''
     while stop not in ['S','N']:
-        stop = input('Deseja continuar? [S/N] ').upper()
+        stop = input('Deseja continuar? [S/N] ').upper()[0]
     if stop == 'N':
-        stop = True
+        break
     else:
         p+=1
-        stop = False
 
 print(f'{len(p18)} Pessoas são maiores de 18 anos \n{len(men)} São homens \n{len(wlt20)} São mulheres com menos de 20')
 
