22609b0fd6fd8f63b5bdac06f1dd0e65aa958230
Talita Araújo <55101326+talitadeoa@users.noreply.github.com>
2021-02-12 01:13:23 -0300

    Update desafio070.py

--- Exercicios-mundo-2/desafio070.py
+++ Exercicios-mundo-2/desafio070.py
@@ -1,18 +1,28 @@
 #Um programa que cria uma lista de compras com nome e preço e ao final informa o total gasto nas compras, quantos produtos custam mais de R$1000 e qual o produto mais barato
 
 product = ''
-price = 0
+p = cheapest = price = 0
 shopping = []
 prices = []
 mt1000 = []
-cheapest = 0
 stop = False
 
-while stop is not False: 
+while stop is not True: 
     product = input('Qual o produto? ')
     shopping.append(product)
-    while price < 0:
-        price = input('Qual o valor do produto? R$')
+    while price == 0:
+        price = int(input('Qual o valor do produto? R$'))
     prices.append(price)
+    if price >= 1000:
+        mt1000.append(p)
+    if price < cheapest:
+        cheapest = p
+    stop = input('Deseja cadastrar mais um produto? [S/N] ').upper()
+    while stop not in ['S','N']:
+        stop = input('Deseja cadastrar mais um produto? [S/N] ').upper()
+    if stop == 'S':
+        p+=1
+    elif stop == 'N':
+        stop = True
 
-print(f'O total das compras foi: {sum(prices)}')        
+print(f'O total das compras foi: {sum(prices)} \n{len(mt1000)} Produtos custaram mais de R$1000 \n o produto mais barato foi {cheapest}')        
