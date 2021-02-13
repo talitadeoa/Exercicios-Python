9f6b86ff145c8f0cd491d73887951f7065ea169d
Talita Araújo <55101326+talitadeoa@users.noreply.github.com>
2021-02-13 14:39:22 -0300

    Update desafio071b.py

--- Exercicios-mundo-2/desafio071b.py
+++ Exercicios-mundo-2/desafio071b.py
@@ -1,7 +1,7 @@
 #Um programa que simula um caixa eletrônico
 #O usuario informa o value que deseja sacar e o programa calcula em quantas notas de $50, $20, $10, ou $1 será feito o saque
 
-value =  int(input('Qual o valor que você deseja sacar? R$')
+value = int(input('Qual o valor que você deseja sacar? R$')
 (total) = value
 (bill) = 50
 (totalbill) = 0
