62d3e430a602c31ac81bf6df461730bc75125093
Talita Araújo <55101326+talitadeoa@users.noreply.github.com>
2021-02-13 14:26:39 -0300

    Update desafio071b.py

--- Exercicios-mundo-2/desafio071b.py
+++ Exercicios-mundo-2/desafio071b.py
@@ -2,8 +2,8 @@
 #O usuario informa o value que deseja sacar e o programa calcula em quantas notas de $50, $20, $10, ou $1 será feito o saque
 
 value =  int(input('Qual o valor que você deseja sacar? R$')
-total - value
-
+total = value
+bill = 50
 
 while True
     if total < 0:
