d542fc4f2c184aca54482749e4d72426c4b7b976
Talita Araújo <55101326+talitadeoa@users.noreply.github.com>
2021-02-14 15:22:23 -0300

    Update desafio073.py

--- Exercicios-mundo-3/desafio073.py
+++ Exercicios-mundo-3/desafio073.py
@@ -1,7 +1,17 @@
-#Um programa que lê os times do brasileirao e informa:
+#Um programa que lê os times do brasileirão e informa:
 #a) Os 5 primeiros
 #b) Os 4 últimos colocados
+#c) Ordem alfabética
+#d) Em qual posição tá o time x
 	
 brasileirao = ('Internacional', 'Flamengo', 'AtléticoMG',' São Paulo','Fluminense' ,'Grêmio' ,'Palmeiras', 'Santos', 'Corinthians', 'Bragantino', 'AthleticoPR', 'Ceará', 'AtléticoGO', 'Sport', 'Fortaleza', 'Bahia', 'Vasco', 'Goiás', 'Coritiba', 'Botafogo')
 
-print(len(brasileirao))
+option = input('''O que você desejaa saber?  
+a) Os 5 primeiros
+b) Os 4 últimos colocados
+c) Ordem alfabética
+d) Em qual posição tá o time_\n
+''').strip().lower()[0]
+
+while option not in 'abcd':
+if option = 
