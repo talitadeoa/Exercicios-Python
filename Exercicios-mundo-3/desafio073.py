bf7f1c99cf3304e7fdb668bdedd5930496fd2796
Talita Araújo <55101326+talitadeoa@users.noreply.github.com>
2021-02-14 15:30:45 -0300

    Update desafio073.py

--- Exercicios-mundo-3/desafio073.py
+++ Exercicios-mundo-3/desafio073.py
@@ -6,12 +6,19 @@
 	
 brasileirao = ('Internacional', 'Flamengo', 'AtléticoMG',' São Paulo','Fluminense' ,'Grêmio' ,'Palmeiras', 'Santos', 'Corinthians', 'Bragantino', 'AthleticoPR', 'Ceará', 'AtléticoGO', 'Sport', 'Fortaleza', 'Bahia', 'Vasco', 'Goiás', 'Coritiba', 'Botafogo')
 
-option = input('''O que você desejaa saber?  
+option = ''
+
+while option not in 'a,b,c,d':
+    option = input('''O que você desejaa saber?  
 a) Os 5 primeiros
 b) Os 4 últimos colocados
 c) Ordem alfabética
 d) Em qual posição tá o time_\n
 ''').strip().lower()[0]
 
-while option not in 'abcd':
-if option = 
+if option == 'a':
+    print(brasileirao[:6])
+elif option == 'b':
+    print(brasileirao[-5:])
+elif option == 'c':
+    print(sorted(brasileirao))
