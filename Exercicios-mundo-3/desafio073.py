da4613c681de53a41da6f5c273e819e17755b74e
Talita Araújo <55101326+talitadeoa@users.noreply.github.com>
2021-02-14 15:16:12 -0300

    Update desafio073.py

--- Exercicios-mundo-3/desafio073.py
+++ Exercicios-mundo-3/desafio073.py
@@ -1,4 +1,6 @@
-
+#Um programa que lê os times do brasileirao e informa:
+#a) Os 5 primeiros
+#b) Os 4 últimos colocados
 	
 brasileirao = ('Internacional', 'Flamengo', 'AtléticoMG',' São Paulo','Fluminense' ,'Grêmio' ,'Palmeiras', 'Santos', 'Corinthians', 'Bragantino', 'AthleticoPR', 'Ceará', 'AtléticoGO', 'Sport', 'Fortaleza', 'Bahia', 'Vasco', 'Goiás', 'Coritiba', 'Botafogo')
 
