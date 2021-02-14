169d71912f6dea7d1854466d8d932b3368606899
Talita Araújo <55101326+talitadeoa@users.noreply.github.com>
2021-02-14 16:30:57 -0300

    Update desafio073.py

--- Exercicios-mundo-3/desafio073.py
+++ Exercicios-mundo-3/desafio073.py
@@ -4,21 +4,22 @@
 #c) Ordem alfabética
 #d) Em qual posição tá o time x
 	
-brasileirao = ('Internacional', 'Flamengo', 'AtléticoMG',' São Paulo','Fluminense' ,'Grêmio' ,'Palmeiras', 'Santos', 'Corinthians', 'Bragantino', 'AthleticoPR', 'Ceará', 'AtléticoGO', 'Sport', 'Fortaleza', 'Bahia', 'Vasco', 'Goiás', 'Coritiba', 'Botafogo')
+brasileirao = ('Internacional', 'Flamengo', 'AtléticoMG','São Paulo','Fluminense' ,'Grêmio' ,'Palmeiras', 'Santos', 'Corinthians', 'Bragantino', 'AthleticoPR', 'Ceará', 'AtléticoGO', 'Sport', 'Fortaleza', 'Bahia', 'Vasco', 'Goiás', 'Coritiba', 'Botafogo')
 
 option = ''
+resume = ''
 
 while option not in 'abcd':
-    option = input('''O que você desejaa saber?  
+    option = input('''O que você deseja saber?  
 a) Os 5 primeiros
 b) Os 4 últimos colocados
 c) Ordem alfabética
-d) Em qual posição tá o time_\n
+d) Em qual posição tá o time x
 ''').strip().lower()[0]
 
 if option == 'a':
-    print(brasileirao[:6])
+    print(brasileirao[:5])
 elif option == 'b':
-    print(brasileirao[-5:])
+    print(brasileirao[-4:])
 elif option == 'c':
     print(sorted(brasileirao))
