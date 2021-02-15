259fddf0a04d129b31f671c409771b613c862343
Talita Araújo <55101326+talitadeoa@users.noreply.github.com>
2021-02-15 18:00:54 -0300

    Update desafio076.py

--- Exercicios-mundo-3/desafio076.py
+++ Exercicios-mundo-3/desafio076.py
@@ -2,5 +2,9 @@
 
 products = ('leite', 3.00, 'arroz', 7.00, 'chocolate', 6.00, 'feijao', 9.00)
 
-for p in products:
-    print(f'')
+print('-'*40)
+print('{: ^40}'.format(' Listagem de produtos '))
+print('-'*40)
+
+for p in products,1:
+    print('{: ^40}'.format(p))
