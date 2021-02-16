ffddf6758299e3d5df9bdcc0e2bd3f31ba683046
Talita Araújo <55101326+talitadeoa@users.noreply.github.com>
2021-02-16 00:59:33 -0300

    Delete jfujyfj.py

--- Exercicios-mundo-3/jfujyfj.py
+++
@@ -1,8 +0,0 @@
-products = ('leite', 3.00, 'arroz', 127.00, 'chocolate', 6.00, 'feijao', 9.00)
-
-for i in products:
-    if type(i) is str:
-        print(f'{i:.<30}', end='')
-    else:
-        print(f'R$ {i:>7.2f}')
-print('--'*20)
