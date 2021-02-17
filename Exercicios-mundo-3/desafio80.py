27fa75fb81491506606ca3b4c2950d11f1a4f376
Talita Araújo <55101326+talitadeoa@users.noreply.github.com>
2021-02-17 18:32:00 -0300

    ,

---
+++ Exercicios-mundo-3/desafio80.py
@@ -0,0 +1,8 @@
+#Um programa que cria uma lista ordenada com inputs do usuario
+
+nums.sort() = []
+for n in range (0,5):
+    num = int(input('Digite um numero '))
+    nums.sort().append(num)
+
+print(nums)
