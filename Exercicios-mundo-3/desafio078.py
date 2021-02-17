b8f58a0d5ddee60dc7c8b30db0128b8870f3bfcb
Talita Araújo <55101326+talitadeoa@users.noreply.github.com>
2021-02-17 15:08:55 -0300

    Create desafio078.py

---
+++ Exercicios-mundo-3/desafio078.py
@@ -0,0 +1,11 @@
+#Um programa que lê 5 valores, armazena em uma lista e ao final mostra o maior e menor e suas respectivas posições na lista
+
+numbers = []
+
+for n in range(0,6):
+    num = int(input('Digite o {n}º número...'))
+    numbers.append(num)
+
+if n == 0:
+    b = num
+    s = num
