5e99b1e40e2699db7ace222093d80313fe1c3a95
Talita Araújo <55101326+talitadeoa@users.noreply.github.com>
2021-02-21 15:03:53 -0300

    .

---
+++ Exercicios-mundo-3/desafio080.py
@@ -0,0 +1,18 @@
+#Um programa que cria uma lista ordenada com inputs do usuario
+
+nums = []
+
+for n in range (0,5):
+    num = int(input('Digite um numero '))
+    if n == 0 or num > nums[-1]:
+        nums.append(num)
+
+    else: 
+        pos = 0
+    while pos < len(nums):
+        if num <= nums[pos]:
+            nums.insert(pos, n)
+            break
+        pos += 1
+
+print(numsor)
--- Exercicios-mundo-3/desafio80.py
+++
@@ -1,8 +0,0 @@
-#Um programa que cria uma lista ordenada com inputs do usuario
-
-nums.sort() = []
-for n in range (0,5):
-    num = int(input('Digite um numero '))
-    nums.sort().append(num)
-
-print(nums)
