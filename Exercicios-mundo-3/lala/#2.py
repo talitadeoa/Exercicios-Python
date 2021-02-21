eae38a349f6f6eda6e80e03bf952077b21c8f9fc
Talita Araújo <55101326+talitadeoa@users.noreply.github.com>
2021-02-21 15:41:09 -0300

    Update #2.py

--- Exercicios-mundo-3/lala/#2.py
+++ Exercicios-mundo-3/lala/#2.py
@@ -1,5 +1,6 @@
 #Programinha que calcula horas
 #Transformar em função
+#opção de fazer com hrs e n so minutos
 
 nowh = int(input('Que horas são? '))
 nowm = int(input('Que minutos são? '))
@@ -14,7 +15,7 @@ if option == 1:
             resm = nowm + plus - 60
 elif option == 0:
     less == int(input('Deseja tirar quantos minutos? '))
-    if nowm + less >= 60:
-        nowh + 1
+    if nowm - less >= 0:
+        nowh - 1
         if nowm + less - 60 > 0:
             resm = nowm + less - 60    
