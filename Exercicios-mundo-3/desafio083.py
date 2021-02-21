71385ef36b2ac32576c667ae2888bf5b45f65bfe
Talita Araújo <55101326+talitadeoa@users.noreply.github.com>
2021-02-21 20:05:39 -0300

    Update desafio083.py

--- Exercicios-mundo-3/desafio083.py
+++ Exercicios-mundo-3/desafio083.py
@@ -1,1 +1,22 @@
-#
+#Um programa que analisa se uma expressão é valida
+
+expression = input('Digite a expressão que deseja analisar: ')
+countl = 0
+countr = 0
+
+for l in expression:
+    if '(' == l:
+        countl += 1
+    elif ')' == l:
+        countr += 1
+    sumrl = (countl+countr)%2
+    elif sumrl!=0 :  
+        print('Expressão inválida')
+    
+    stop = ''
+    while stop not in ['S','N']:
+        stop = input('Deseja continuar? [S/N] ').strip().upper()[0]
+    if stop in 'N':
+        break
+    
+print('Expressão válida!')
