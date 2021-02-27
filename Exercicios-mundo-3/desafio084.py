f87e8ae9daac14a2aa0e53cf0e95b445f017aef1
Talita Araújo <55101326+talitadeoa@users.noreply.github.com>
2021-02-27 17:09:11 -0300

    Update desafio084.py

---
+++ Exercicios-mundo-3/desafio084.py
@@ -0,0 +1,15 @@
+#Um programa que lê nome e peso e ao final mostra quantas pessoas foram cadastradas, quais foram as mais pesadas e menos pesadas
+
+people = []
+heaviest = []
+thinner = []
+person = 1
+thin
+fatty 
+while True:
+    name = str(input(f'Digite o nome da {person}ª pessoa '))
+    weight = float(input(f'Digite o peso da {person}ª pessoa '))
+    people.append(name,weight)
+    person += 1
+    if person == 1:
+        heaviest.append(weight)
