a072fd74aafcd586d7548a2b5ccb2e6b58cc839d
Talita Araújo <55101326+talitadeoa@users.noreply.github.com>
2021-03-01 19:09:49 -0300

    Update desafio084.py

--- Exercicios-mundo-3/desafio084.py
+++ Exercicios-mundo-3/desafio084.py
@@ -1,29 +1,23 @@
 #Um programa que lê nome e peso e ao final mostra quantas pessoas foram cadastradas, quais foram as mais pesadas e menos pesadas
-#Tente outra forma usando o clear
 
+temp = []
 people = []
-weights = []
-heaviest = 0
-thinner = 0
-person = 1
-thin = []
-fatty  = []
-
+weight = []
 while True:
-    name = str(input(f'Digite o nome da {person}ª pessoa '))
-    people.append(name)
-    weight = int(input(f'Digite o peso da {person}ª pessoa '))
-    weights.append(weight)
-    if weight > heaviest:
-        heaviest = weight
-    for count, w in enumerate(weights):
-         if w == heaviest:
-            fatty.append(people[count])
-    stop = ''
-    while stop not in ['S','N']:
-        stop = input('Deseja continuar? [S/N] ').strip().upper()[0]
-    if stop in 'N':
+    temp.append(str(input('Nome: ')))
+    temp.append(float(input('Peso: ')))
+    people.append(temp[:])
+    weight.append(temp[1])
+    temp.clear()
+    resp = str(input('Deseja continuar? [S/N] '))
+    if resp in 'Nn':
         break
-    person += 1
-print(f'''Foram adcionadas {person} pessoas
-A mais pesada tem {heaviest} Kg. São: {fatty} ''')
+print(f'Você cadastrou {len(people)} pessoas')
+print(f'O maior peso foi de {max(weight):.2f}Kg. Peso de ', end='')
+for p in people:
+    if p[1] == max(weight):
+        print(f'{p[0]} ', end='')
+print(f'\nO menor peso foi de {min(weight):.2f}Kg. Peso de ', end='')
+for p in people:
+    if p[1] == min(weight):
+        print(f'{p[0]} ', end='')
