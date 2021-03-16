3746569a02b4bef50e56d7d9376ce4a2376f8f33
Talita Araújo <55101326+talitadeoa@users.noreply.github.com>
2021-03-15 22:25:12 -0300

    Update desafio085.py

--- Exercicios-mundo-3/desafio085.py
+++ Exercicios-mundo-3/desafio085.py
@@ -1,10 +1,13 @@
 #Um programa que lê 7 valores, armazena em uma lista composta, separada por pares e ímpares, ao final mostra números pares e ímpares separadamente em ordem crescente
 
 values = [[],[]]
+temp = []
 
 for i in range(0,7):
     temp.append(int(input('Digite um valor_')))
     if temp[i]%2==0:
-        values.insert(0, temp[i])
+        values.insert(values[0],0), temp[i])
+    else:
+        values.insert(values[1],0), temp[i])      
 
 print(values)
