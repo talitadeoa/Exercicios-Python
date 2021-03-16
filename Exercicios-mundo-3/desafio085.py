1f0d955ac9bc52469b2d6c4216869beac10e18dc
Talita Araújo <55101326+talitadeoa@users.noreply.github.com>
2021-03-15 22:36:51 -0300

    Update desafio085.py

--- Exercicios-mundo-3/desafio085.py
+++ Exercicios-mundo-3/desafio085.py
@@ -6,8 +6,8 @@ temp = []
 for i in range(0,7):
     temp.append(int(input('Digite um valor_')))
     if temp[i]%2==0:
-        values.insert(values[0],0), temp[i])
+        values.insert(values[0][0], temp[i])
     else:
-        values.insert(values[1],0), temp[i])      
+        values.insert(values[1],[0], temp[i])      
 
 print(values)
