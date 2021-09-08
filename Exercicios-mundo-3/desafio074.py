c7e35da7d0d21332a2a445778ea2a128c64b8a21
Talita Araújo <55101326+talitadeoa@users.noreply.github.com>
2021-09-08 16:20:02 -0300

    ,

--- Exercicios-mundo-3/desafio074.py
+++ Exercicios-mundo-3/desafio074.py
@@ -2,14 +2,10 @@
 
 from random import randint
 
-a= randint(0,10)
-b= randint(0,10)
-c= randint(0,10)
-d= randint(0,10)
-e= randint(0,10)
+numbers = ((randint(0,10)),(randint(0,10)),(randint(0,10)),(randint(0,10)),(randint(0,10)))
 
-numbers = (a,b,c,d,e)
-
-print(numbers)
-print(f'maior: {max(numbers)}' )
+print(f'Os números selecionados foram:', end='')
+for n in numbers:
+    print(f'{n} ', end='')
+print(f'\nmaior: {max(numbers)}' )
 print(f'menor: {min(numbers)}' )
--- Exercicios-mundo-3/desafio085.py
+++ Exercicios-mundo-3/desafio085.py
@@ -1,14 +1,14 @@
 #Um programa que lê 7 valores, armazena em uma lista composta, separada por pares e ímpares, ao final mostra números pares e ímpares separadamente em ordem crescente
 
-values = [[],[]]
 temp = []
+values = [[],[]]
 
-for i in range(0,4):
-    temp.append(int(input('Digite um valor_')))
+for i in range(0,7):
+    temp.append(int(input(f'Digite o {i+1}º valor: ')))
     if temp[i]%2==0:
         values[0].append(temp[i])
     else:
         values[1].append(temp[i])      
 values[0].sort()
 values[1].sort()
-print(values) 
+print(f'\nOs valores pares são: {values[0]} \nOs valores ímpares são: {values[1]}') 
