cc9e25de32c2b7cf4656b0c663e8387c9cca98bc
Talita Araújo <55101326+talitadeoa@users.noreply.github.com>
2021-09-08 17:19:00 -0300

    Update desafio086.py

--- Exercicios-mundo-3/desafio086.py
+++ Exercicios-mundo-3/desafio086.py
@@ -1,2 +1,9 @@
 #Programa que cria uma matriz 3x3 com números inseridos pelo usuário
 
+array = [[],[],[]]
+
+for i in range (0,9):
+    value = int(input(f'Digite o número para a posição [,]'))
+    
+
+print(f'{array[0]} \n{array[1]} \n{array[2]}')
