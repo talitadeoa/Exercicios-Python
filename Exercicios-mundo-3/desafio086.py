4035cc6e289942faed8ba07e883d837d6ab895c1
Talita Araújo <55101326+talitadeoa@users.noreply.github.com>
2021-09-08 17:33:12 -0300

    Update desafio086.py

--- Exercicios-mundo-3/desafio086.py
+++ Exercicios-mundo-3/desafio086.py
@@ -1,9 +1,14 @@
 #Programa que cria uma matriz 3x3 com números inseridos pelo usuário
 
-array = [[],[],[]]
+array = [[[],[],[]],[[],[],[]],[[],[],[]]]
 
 for i in range (0,9):
     value = int(input(f'Digite o número para a posição [,]'))
-    
+    if i <= 2:
+        array[0].append(value)
+    if i > 2 and i <= 5:
+        array[1].append(value)        
+    if i > 5:
+        array[2].append(value)  
 
 print(f'{array[0]} \n{array[1]} \n{array[2]}')
