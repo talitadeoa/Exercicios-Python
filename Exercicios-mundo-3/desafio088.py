f3fbfe548937f03a033a327dd04ba0338506aba6
Talita Araújo <55101326+talitadeoa@users.noreply.github.com>
2021-09-23 17:52:40 -0300

    Update desafio088.py

---
+++ Exercicios-mundo-3/desafio088.py
@@ -0,0 +1,7 @@
+#Gerador de apostas da mega-sena. Um programa que gera x listas com 6 posições de inteiros de 1 a 60. 
+
+
+bets = int(input('Quantas apostas deseja gerar?'))
+bet = []
+
+for i in range(0,bets):
