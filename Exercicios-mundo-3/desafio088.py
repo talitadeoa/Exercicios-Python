931a65c4638f61b7b0f5b3a7b9c8b2ac3b602700
Talita Araújo <55101326+talitadeoa@users.noreply.github.com>
2021-09-23 19:29:13 -0300

    Update desafio088.py

--- Exercicios-mundo-3/desafio088.py
+++ Exercicios-mundo-3/desafio088.py
@@ -1,4 +1,5 @@
 #Gerador de apostas da mega-sena. Um programa que gera x listas com 6 posições de inteiros de 1 a 60. 
+
 from random import randint 
 
 bets = int(input('Quantas apostas deseja gerar? '))
@@ -10,6 +11,7 @@ for i in range(0,bets):
         num = randint(1,60)
         if num not in temp:
             temp.append(num)
+    temp.sort()        
     bet.append(temp[:])
     temp.clear()
     print(f'\nJogo {i+1}: {bet[i]}')
