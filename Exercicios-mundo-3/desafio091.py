c05dc40d3f4f439ef855585d13829998aa3504ed
Talita Araújo <55101326+talitadeoa@users.noreply.github.com>
2021-10-06 18:45:34 -0300

    ,

---
+++ Exercicios-mundo-3/desafio091.py
@@ -0,0 +1,9 @@
+#Programa que sorteia números aleatórios para 4 jogadores, os dados são armazenados em um dicionário ordenado que é apresentado ao fim do programa
+
+import random
+
+game = []
+player = {}
+
+for i in range(4):
+    player[score]: random.randint(1,6)
