052ddae54c7663a516dfb2386b0cef81528ff60a
Talita Araújo <55101326+talitadeoa@users.noreply.github.com>
2021-10-06 22:52:45 -0300

    Update desafio092.py

---
+++ Exercicios-mundo-3/desafio092.py
@@ -0,0 +1,2 @@
+#Programa que recebe os valores nome, data de nascimento e carteira de trabalho e calcula a idade, se a carteira de trabalho for diferente de 0 também recebe os valores de ano de contratação e salário e calcula a idade que o usuario irá se aposentar
+
