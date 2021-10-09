679fdb052d0fd64a38ff0a2fd99cd7fb8963fe9f
Talita Araújo <55101326+talitadeoa@users.noreply.github.com>
2021-10-09 19:58:43 -0300

    Update desafio095.py

--- Exercicios-mundo-3/desafio095.py
+++ Exercicios-mundo-3/desafio095.py
@@ -1,1 +1,13 @@
-#
+#Aprimoramento do desafio 093, permmite cadastrar varios jogadores incluindo um sistema de visualização de detalhes do aproveitamento de cada jogador 
+
+player = {'name': str(input('Nome: ')), 'matches': int(input('Partidas jogadas: '))}
+goals = []
+
+for i in range(player['matches']):
+    goals.append(int(input(f'Quantos gols na {i+1}ª partida ')))
+    player['goals'] = goals[:]
+
+print(f"\nO jogador {player['name']} participou em {player['matches']} partidas")
+for i in range(player['matches']):
+    print(f'Na {i+1}ª partida => {goals[i]} gols')
+print(f'Totalizando {sum(player["goals"])} gols no campeonato')
