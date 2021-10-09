11e8ad6c6b758c5b5fdf5050a3e97f98c308ea7e
Talita Araújo <55101326+talitadeoa@users.noreply.github.com>
2021-10-09 20:51:32 -0300

    Update desafio095.py

--- Exercicios-mundo-3/desafio095.py
+++ Exercicios-mundo-3/desafio095.py
@@ -1,11 +1,22 @@
 #Aprimoramento do desafio 093, permmite cadastrar varios jogadores incluindo um sistema de visualização de detalhes do aproveitamento de cada jogador 
 
-player = {'name': str(input('Nome: ')), 'matches': int(input('Partidas jogadas: '))}
-goals = []
+players = []
 
-for i in range(player['matches']):
-    goals.append(int(input(f'Quantos gols na {i+1}ª partida ')))
-    player['goals'] = goals[:]
+while True:
+    player = {'name': str(input('Nome: ')), 'matches': int(input('Partidas jogadas: '))}
+    goals = []
+    for i in range(player['matches']):
+        goals.append(int(input(f'Quantos gols na {i+1}ª partida ')))
+        player['goals'] = goals[:]
+    players.append(player.copy())    
+    while True:
+        status = str(input('Deseja continuar? [S/N] ')).upper()[0]
+        if status in 'SN':
+            break
+        print('Por favor digite apenas S ou N.')
+    if status == 'N':
+        break   
+
 
 print(f"\nO jogador {player['name']} participou em {player['matches']} partidas")
 for i in range(player['matches']):
