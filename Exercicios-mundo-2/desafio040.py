86635780de9e0b3704c8830c9143803c52c5bc8c
Talita Araújo <55101326+talitadeoa@users.noreply.github.com>
2021-10-09 19:20:43 -0300

    .

--- Exercicios-mundo-2/desafio040.py
+++ Exercicios-mundo-2/desafio040.py
@@ -2,13 +2,13 @@
 
 n1 = float(input('Qual foi sua primeira nota? '))
 n2 = float(input('E a segunda? '))
-media = (n1+n2)/2
+average = (n1+n2)/2
 
 print('Ok, estou analisando...')
 
-if media < 5:
-    print('Foi mal, você está reprovado, sua média foi {}. Tente estudar mais na próxima! Boa sorte.'.format(media))
-elif media >= 7:
-    print('Parabéns! sua média foi {}, você está aprovado!'.format(media))
+if average < 5:
+    print('Foi mal, você está reprovado, sua média foi {}. Tente estudar mais na próxima! Boa sorte.'.format(average))
+elif average >= 7:
+    print('Parabéns! sua média foi {}, você está aprovado!'.format(average))
 else:
-    print('Sua média é {}, você está de recuperação, estude e boa sorte!'.format(media))
+    print('Sua média é {}, você está de recuperação, estude e boa sorte!'.format(average))
--- Exercicios-mundo-3/desafio094.py
+++ Exercicios-mundo-3/desafio094.py
@@ -1,5 +1,36 @@
-#Programa que recebe dados de várias pessoas, como nome, idade e sexo, por fim cria uma lista e informa a quantidade de pessoas cadastradas, a média da idade entre elas e lista as pessoas com idade acima da média
+#Programa que recebe dados de várias pessoas, como nome, idade e sexo, por fim cria uma lista e informa a quantidade de pessoas cadastradas, as mulheres e a média da idade entre elas e lista as pessoas com idade acima da média
 
-pessoa = {}
-pessoas = []
-status = True
+person= {}
+people = []
+status = '0'
+sum = media = 0
+acima_media = []
+
+while True:
+    person['name'] = str(input('Nome: '))
+    person['age'] = int(input('Idade: '))
+    sum += person['age']
+    while True:
+        person['sex'] = str(input('Sexo [F/M]: ')).upper()[0]
+        if person['sex'] in 'FM:':
+            break
+        print('Por favor digite apenas M ou F.')
+    person.append(person.copy())
+    while True:
+        status = str(input('Deseja continuar? [S/N] ')).upper()[0]
+        if status in 'SN':
+            break
+        print('Por favor digite apenas S ou N.')
+    if status == 'N':
+        break
+media = (sum/len(person)) 
+
+print(f'''Foram cadastradas {len(person)} pessoas
+A média de idade entre elas é {media:.1f} anos
+As mulheres cadastradas são 
+As pessoas que tem idade acima da média são: ''', end='')
+for p in person:
+    if person[i]['age'] > media:
+        acima_media.append(person[i]['name'].copy())    
+
+print(acima_media)
