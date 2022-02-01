0a2ff9efca60d91ab162ea0231d90f8135d28d18
Talita Araújo <55101326+talitadeoa@users.noreply.github.com>
2022-02-01 20:41:05 -0300

    Update desafio095.py

--- Exercicios-mundo-3/desafio095.py
+++ Exercicios-mundo-3/desafio095.py
@@ -4,12 +4,13 @@ team = []
 player = dict()
 
 while True:
+    player.clear()     
     player = {'name': str(input('Nome: ')), 'matches': int(input('Partidas jogadas: ')), 'goals':[], 'total': 0}  
     for i in range(player['matches']):
         player['goals'].append(int(input(f'    Quantos gols na {i+1}ª partida ')))
         player['total'] = sum(player['goals'])
     team.append(player.copy())   
-    player.clear() 
+    
     while True:
         status = str(input('Deseja continuar? [S/N] ')).upper()[0]
         if status in 'SN':
@@ -23,14 +24,21 @@ print('cod ', end='')
 for k in player.keys():
     print(f'{k:<15}', end='') 
 print()    
-print('-'*40)
+print('--'*30)
 for c, player in enumerate(team):
     print(f'{c:>3} ', end='')
     for d in player.values():
         print(f'{str(d):<15}', end='') 
     print()           
-print('-----')
+print('--'*30)
     
-#while True:
-#    status = str(input('Para visualizar os dados de um jogador específico digite seu número '))
-#    print('Por favor digite apenas S ou N.')
+while True:
+    pick = int(input('Para visualizar os dados de um jogador específico digite seu número (999 para finalizar) '))
+    if pick == 999:
+        break
+    if pick > len(team):
+        print(f'O/A jogador/a {pick} não existe, tente mais uma vez')
+    else:
+        print(f"Jogador/a {team[pick]['name']}")
+    for i, g in enumerate(team[pick]['goals']):
+        print(f'     Na partida {i+1} fez {g} gols')
