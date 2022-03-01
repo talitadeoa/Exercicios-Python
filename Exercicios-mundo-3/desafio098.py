91dd12973bc6feaca101c9b5fb80f08d28e1abd5
Talita Araújo <55101326+talitadeoa@users.noreply.github.com>
2022-03-01 13:43:28 -0300

    .

---
+++ Exercicios-mundo-3/desafio098.py
@@ -0,0 +1,16 @@
+#Função contador que recebe 3 parametros, início, fim e passe, com exemplos e contador personalizado
+#1 a 10 de 1 em 1
+#10 a 0 de 2 em 2
+#personalizado
+
+from time import sleep
+
+def contador(ini,fim,pas):
+    print(f'Contagem de {ini} até {fim} de {pas} em {pas}:')
+    for i in range(ini,fim,pas):
+        print(f'{i} ', end='')
+        sleep(0.5)
+    print('Fim!')
+
+#contador(1,10,1)
+contador(10,0,2)
