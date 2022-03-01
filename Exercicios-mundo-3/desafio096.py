7d20950359364853d9f7347dfd57fb3ce0d70109
Talita Araújo <55101326+talitadeoa@users.noreply.github.com>
2022-03-01 13:18:11 -0300

    .

---
+++ Exercicios-mundo-3/desafio096.py
@@ -0,0 +1,9 @@
+#Função para calcular área
+
+def area(b,h):
+    A = b * h
+    print(f'''\nLargura = {b}m
+Comprimento = {h}m
+A área de um terreno {b}x{h} é {A}m²''')
+
+area(float(input('Qual a largura do terreno em metros? ')),float(input('Qual o comprimento do terreno em metros? ')))
---
+++ Exercicios-mundo-3/desafio097.py
@@ -0,0 +1,9 @@
+#Uma função que recebe um texto e o transofmra em um título adaptável
+
+def escrever(txt):
+    bar = int(len(txt)+2)
+    print(f'~'* bar)
+    print(f'{txt:^{bar}}')
+    print(f'~'* bar)
+    
+escrever(str(input('Digite um título: ')))
