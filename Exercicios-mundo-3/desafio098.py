2b4a41041e5d2914a43a2c6771c2b914f05a2ec0
Talita Araújo <55101326+talitadeoa@users.noreply.github.com>
2022-03-03 12:48:41 -0300

    Update desafio098.py

--- Exercicios-mundo-3/desafio098.py
+++ Exercicios-mundo-3/desafio098.py
@@ -5,12 +5,22 @@
 
 from time import sleep
 
+def linha():
+    print('--'*20)
+    
 def contador(ini,fim,pas):
     print(f'Contagem de {ini} até {fim} de {pas} em {pas}:')
-    for i in range(ini,fim,pas):
-        print(f'{i} ', end='')
-        sleep(0.5)
-    print('Fim!')
+    if ini < fim:
+        for i in range(ini,fim,pas):
+            print(f'{i} ', end='', flush=True)
+            sleep(0.5)
+        print('Fim!')
+    else:
+        cont = ini
+        while cont 
 
 contador(1,10,1)
 contador(10,0,2)
+linha()
+print('Agora é sua vez de escolher a contagem!')
+contador(int(input('Início:')),int(input('Fim:')),int(input('Passe:')))
