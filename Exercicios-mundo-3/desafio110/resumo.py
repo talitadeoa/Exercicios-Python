e8be0058b8c4fbdd495b097d94654536bee54f8b
Talita <55101326+talitadeoa@users.noreply.github.com>
2022-04-21 17:14:47 -0300

    .

--- Exercicios-mundo-3/desafio110/resumo.py
+++
@@ -1,9 +0,0 @@
-
-import moeda
-
-p = float(input('Digite o preço: R$'))
-
-print(f'A metade de {moeda.moeda(p)} é {moeda.metade(p,True)}')
-print(f'O dobro de {moeda.moeda(p)} é {moeda.dobro(p,True)}')
-print(f'Aumentado 13% de {moeda.moeda(p)}, temos {moeda.aumentar(p,13,True)}')
-print(f'Diminuindo 14% de {moeda.moeda(p)}, temos {moeda.diminuir(p,14,True)}')
--- Exercicios-mundo-3/desafio110/teste.py
+++ Exercicios-mundo-3/desafio110/teste.py
@@ -1,7 +1,8 @@
-
 import moeda
 
+
 p = float(input('Digite o preço: R$'))
+moeda.resumo(p,43,17)
 
 print(f'A metade de {moeda.moeda(p)} é {moeda.metade(p,True)}')
 print(f'O dobro de {moeda.moeda(p)} é {moeda.dobro(p,True)}')
